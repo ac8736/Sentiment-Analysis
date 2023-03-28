1bfcbd4913c96ed0b2c759870e87a8bfa3041d99
Kewl <87680132+ac8736@users.noreply.github.com>
2023-03-28 19:00:52 -0400

    Milestone 2 (#8)
    * merge (#3)
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * adding sentiment analysis for dummy text
    
    * update version
    
    * testing deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 from transformers import pipeline
 
-classifier = pipeline(task="sentiment-analysis")
+# classifier = pipeline(task="sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 st.title("Sentiment Analysis App")
 
@@ -10,7 +10,9 @@ NEGATIVE = "You are a terrible person!"
 
 st.caption(POSITIVE)
 if st.button("Get sentiment", key=1):
-    st.write(classifier(POSITIVE)[0]['label'])
+    # st.write(classifier(POSITIVE)[0]['label'])
+    st.write("POSITIVE")
 st.caption(NEGATIVE)
 if st.button("Get sentiment", key=2):
-    st.write(classifier(NEGATIVE)[0]['label'])
+    st.write("POSITIVE")
+    # st.write(classifier(NEGATIVE)[0]['label'])
