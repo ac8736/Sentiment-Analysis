7f33f1af83ae7894263eea573efdbd8339608c76
Kewl <87680132+ac8736@users.noreply.github.com>
2023-03-28 18:13:35 -0400

    Milestone 2 (#4)
    * merge (#3)
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * adding sentiment analysis for dummy text

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-import easyocr as ocr  #OCR
-import streamlit as st  #Web App
-from PIL import Image #Image Processing
-import numpy as np #Image Processing 
+import streamlit as st 
+from transformers import pipeline
 
-#title
-st.title("Easy OCR - Extract Text from Images")
+classifier = pipeline(task="sentiment-analysis")
+
+st.title("Sentiment Analysis App")
+
+POSITIVE = "You are a great person!"
+NEGATIVE = "You are a terrible person!"
+
+st.caption(POSITIVE)
+if st.button("Get sentiment", key=1):
+    st.write(classifier(POSITIVE)[0]['label'])
+st.caption(NEGATIVE)
+if st.button("Get sentiment", key=2):
+    st.write(classifier(NEGATIVE)[0]['label'])
