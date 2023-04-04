9ae4c2fe7ca6eb4af4ddf4e0d46651bb54757224
Kewl <87680132+ac8736@users.noreply.github.com>
2023-04-04 15:10:51 -0400

    adding the models

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st 
-# from transformers import pipeline
-
-# classifier = pipeline(task="sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+from transformers import pipeline
 
 st.title("Sentiment Analysis App")
 
@@ -9,17 +7,23 @@ text = st.text_input("Input text to get sentiment.")
 
 model = st.selectbox(
     'Select the model you want to use below.',
-    ("distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "papluca/xlm-roberta-base-language-detection", "ProsusAI/finbert"))
+    ("distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis", "ProsusAI/finbert"))
+
+classifier = pipeline(task="sentiment-analysis", model=model)
 
 st.write('You selected:', model)
 
 if st.button("Get Sentiment"):
-    st.write("The sentiment is .")
-# st.caption(POSITIVE)
-# if st.button("Get sentiment", key=1):
-#     # st.write(classifier(POSITIVE)[0]['label'])
-#     st.write("POSITIVE")
-# st.caption(NEGATIVE)
-# if st.button("Get sentiment", key=2):
-#     st.write("POSITIVE")
-#     # st.write(classifier(NEGATIVE)[0]['label'])
+    prediction = classifier(text)[0]["label"]
+    if model == "distilbert-base-uncased-finetuned-sst-2-english":
+        sentiment = prediction
+        st.write(f"The sentiment is {sentiment}.")
+    elif model == "cardiffnlp/twitter-roberta-base-sentiment":
+        sentiment = "NEGATIVE" if prediction == "LABEL_0" else "POSITIVE" if prediction == "LABEL_2" else "NEUTRAL"
+        st.write(f"The sentiment is {sentiment}.")
+    elif model == "finiteautomata/bertweet-base-sentiment-analysis":
+        sentiment = "NEGATIVE" if prediction == "NEG" else "POSITIVE" if prediction == "POS" else "NEUTRAL"
+        st.write(f"The sentiment is {sentiment}.")
+    elif model == "ProsusAI/finbert":
+        sentiment = prediction.upper()
+        st.write(f"The sentiment is {sentiment}.")
