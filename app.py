2d8035c7e8318512b522ecbc18507d5e4cf89d56
Kewl <87680132+ac8736@users.noreply.github.com>
2023-04-21 12:10:58 -0400

    Milestone 3 (#12)
    * attempts to finetune
    
    * adding fine tuned model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 from transformers import pipeline
+import pandas as pd
 
 st.title("Sentiment Analysis App")
 
@@ -7,23 +8,29 @@ text = st.text_area("Input text to get sentiment.", "You are a nice person!")
 
 model = st.selectbox(
     'Select the model you want to use below.',
-    ("distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis", "ProsusAI/finbert"))
+    ("ac8736/toxic-tweets-fine-tuned-distilbert", "distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis", "ProsusAI/finbert"))
 
 classifier = pipeline(task="sentiment-analysis", model=model)
 
 st.write('You selected:', model)
 
 if st.button("Get Sentiment"):
-    prediction = classifier(text)[0]["label"]
-    if model == "distilbert-base-uncased-finetuned-sst-2-english":
-        sentiment = prediction
-        st.write(f"The sentiment is {sentiment}.")
-    elif model == "cardiffnlp/twitter-roberta-base-sentiment":
-        sentiment = "NEGATIVE" if prediction == "LABEL_0" else "POSITIVE" if prediction == "LABEL_2" else "NEUTRAL"
-        st.write(f"The sentiment is {sentiment}.")
-    elif model == "finiteautomata/bertweet-base-sentiment-analysis":
-        sentiment = "NEGATIVE" if prediction == "NEG" else "POSITIVE" if prediction == "POS" else "NEUTRAL"
-        st.write(f"The sentiment is {sentiment}.")
-    elif model == "ProsusAI/finbert":
-        sentiment = prediction.upper()
-        st.write(f"The sentiment is {sentiment}.")
+    if model != "ac8736/toxic-tweets-fine-tuned-distilbert":
+        prediction = classifier(text)[0]["label"]
+        if model == "distilbert-base-uncased-finetuned-sst-2-english":
+            sentiment = prediction
+            st.write(f"The sentiment is {sentiment}.")
+        elif model == "cardiffnlp/twitter-roberta-base-sentiment":
+            sentiment = "NEGATIVE" if prediction == "LABEL_0" else "POSITIVE" if prediction == "LABEL_2" else "NEUTRAL"
+            st.write(f"The sentiment is {sentiment}.")
+        elif model == "finiteautomata/bertweet-base-sentiment-analysis":
+            sentiment = "NEGATIVE" if prediction == "NEG" else "POSITIVE" if prediction == "POS" else "NEUTRAL"
+            st.write(f"The sentiment is {sentiment}.")
+        elif model == "ProsusAI/finbert":
+            sentiment = prediction.upper()
+            st.write(f"The sentiment is {sentiment}.")
+    else:
+        prediction = classifier(text)[0]
+        df = pd.DataFrame([text, prediction['label'], f"{round(prediction['score']*100, 3)}%"])#, columns=["Tweet/Text", "Highest Toxicity", "Probability"])
+        st.table(df)
+        
