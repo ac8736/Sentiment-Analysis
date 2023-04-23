af65063a9a69824e408c45f9bd1cc3723712acda
Kewl <87680132+ac8736@users.noreply.github.com>
2023-04-23 14:19:45 -0400

    Milestone 3 (#14)
    * attempts to finetune
    
    * adding fine tuned model
    
    * adding msg
    
    * changing table

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st 
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 import pandas as pd
+import torch
+
+def map_label(prediction):
+    labels = ["toxic", "severe toxic", "obscene", "threat", "insult", "identity hate"]
+    output = []
+    for predict, labels in (zip(prediction, labels)):
+        output.append({'label': labels, 'score': predict})
+    return output
+
+def score(item):
+    return item['score']
 
 st.title("Sentiment Analysis App")
 
@@ -10,12 +21,11 @@ model = st.selectbox(
     'Select the model you want to use below.',
     ("ac8736/toxic-tweets-fine-tuned-distilbert", "distilbert-base-uncased-finetuned-sst-2-english", "cardiffnlp/twitter-roberta-base-sentiment", "finiteautomata/bertweet-base-sentiment-analysis", "ProsusAI/finbert"))
 
-classifier = pipeline(model=model)
-
 st.write('You selected:', model)
 
 if st.button("Get Sentiment"):
     if model != "ac8736/toxic-tweets-fine-tuned-distilbert":
+        classifier = pipeline(model=model)
         prediction = classifier(text)[0]["label"]
         if model == "distilbert-base-uncased-finetuned-sst-2-english":
             sentiment = prediction
@@ -30,8 +40,15 @@ if st.button("Get Sentiment"):
             sentiment = prediction.upper()
             st.write(f"The sentiment is {sentiment}.")
     else:
-        prediction = classifier(text)[0]
-        df = pd.DataFrame([text, prediction['label'], f"{round(prediction['score']*100, 3)}%"])#, columns=["Tweet/Text", "Highest Toxicity", "Probability"])
+        classifier = AutoModelForSequenceClassification.from_pretrained(model)
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        text_token = tokenizer(text, return_tensors="pt")
+        output = classifier(**text_token)
+        prediction = torch.sigmoid(output.logits)*100
+        prediction = prediction.detach().numpy().tolist()[0]
+        labels = map_label(prediction)
+        labels.sort(key=score, reverse=True)
+        df = pd.DataFrame([(text, labels[0]['label'], f"{round(labels[0]['score'], 3)}%", labels[1]['label'], f"{round(labels[1]['score'], 3)}%")], columns=('tweet/text','label 1', 'score 1', 'label 2', 'score 2'))
         st.table(df)
         st.write("Visit https://huggingface.co/ac8736/toxic-tweets-fine-tuned-distilbert for more information about the model and to view all outputs.")
         
